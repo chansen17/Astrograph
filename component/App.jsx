7022e952cab71646f1d755091de0bcdaa0d1c242
Chris Hansen <Chansenwebdev@gmail.com>
2020-06-18 17:58:18 -0700

    init commit

---
+++ component/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState} from 'react';
+import ReactDOM from 'react-dom';
+
+import API_KEY from '../js/api';
+
+
+function App() {
+    
+
+    return (
+        <div className="container-fluid">
+            
+        </div>
+    )
+};
+
+ReactDOM.render(
+<App/>,
+document.getElementById('root')
+);
